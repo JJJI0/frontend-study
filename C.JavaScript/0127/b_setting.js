0bdddf66889b502465adf4628fd829d857774b32
JJJI0 <starat6742@gmail.com>
2024-02-08 16:43:35 +0900

    20240208 sjy

---
+++ C.JavaScript/0127/b_setting.js
@@ -0,0 +1,10 @@
+//! 자바스크립트 개발 환경 설정
+
+// 코드작성 및 확인
+// VSCode설치, 구글 크롬
+// - 개발자 도구(Console창) : F12 / ctrl + shift + i
+
+// 콘솔 창 출력
+// : 간단한 코드와 결괏값을 출력, 에러 확인
+
+console.log("Hello JavaScript");
